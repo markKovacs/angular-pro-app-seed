7a2cc824839dd744b17152fa8e941e606028311b
Mark Kovacs <kovacsmark89@gmail.com>
2018-12-20 14:41:24 +0100

    Temp commit

--- src/health/meals/containers/meals/meals.component.ts
+++ src/health/meals/containers/meals/meals.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
-import { MealsService } from '../../../shared/services/meals/meals.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MealsService, Meal } from '../../../shared/services/meals/meals.service';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from 'store';
 
 @Component({
   selector: 'meals',
   styleUrls: ['./meals.component.scss'],
   template: `
-    <div>Meals</div>
+    <div>
+      {{ meals$ | async | json }}
+    </div>
   `
 })
-export class MealsComponent {
+export class MealsComponent implements OnInit, OnDestroy {
+
+  meals$: Observable<Meal[]>;
+  subscription: Subscription;
 
   constructor(
+    private store: Store,
     private mealsService: MealsService
   ) { }
+
+  ngOnInit() {
+    this.meals$ = this.store.select<Meal[]>('meals');
+    this.subscription = this.mealsService.meals$.subscribe();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
